74228ab30e8452fdfab1e06d1c7e4760005b1958
chenheling <6710582@qq.com>
2015-10-26 10:53:25 +0800

    10.26

--- Algorithms.js
+++ Algorithms.js
@@ -6,7 +6,7 @@ var Algorithms = {};
  * @param  {[type]} desc [排序类型]
  * @return {[type]}      [排序的数组]
  */
-Algorithms.insert_sort = function(arr, desc) { 
+Algorithms.insert_sort = function(arr, desc) {
         var nArr = arr,
             temp, j;
         for (var i = 1; i < nArr.length; i++) {
@@ -37,14 +37,17 @@ Algorithms.insert_sort = function(arr, desc) {
      * @param  {[type]} r   [description]
      * @return {[type]}     [description]
      */
-Algorithms.merge_sort = function(A) { 
-	var p=0,q=A.length/2-1,r=A.length;
-	
-    return (function (A, p, q, r) {
+Algorithms.merge_sort = function(A) {
+    var p = 0,
+        q = A.length / 2 - 1,
+        r = A.length;
+
+    return (function(A, p, q, r) {
         var n1 = q - p + 1;
-        var n2 = r - q -1;
-        var L = A.slice(p,q+1),
-            R = A.slice(q+1,r+1); 
+        var n2 = r - q - 1;
+        var L = A.slice(p, q + 1),
+            R = A.slice(q + 1, r);
+
         // for (var i = 0; i < n1; i++) {
         //     L[i] = A[p + i];
         // };
@@ -53,38 +56,74 @@ Algorithms.merge_sort = function(A) {
         // };
         //arr.slice(p,q)
         // arr.slice(q,r)
-        //L[n1] = 'null';
-        //R[n2] = 'null';
+        L[n1] = 'null';
+        R[n2] = 'null';
         var i = j = 0;
-        for (var k = p; k <= r; k++) { 
-        	if (L[i] != 'null') {L[i] = parseInt(L[i])};
-        	if (R[j] != 'null') {R[j] = parseInt(R[j])};
+        for (var k = p; k < r; k++) {
+
+            if (L[i] == 'null' || R[j] == 'null') {
+                if (L[i] == 'null') {
+                    A[k] = R[j];
+                };
+                if (R[j] == 'null') {
+                    A[k] = L[i];
+                };
+                break;
+            };
+
+
+            L[i] = parseInt(L[i]);
+            R[j] = parseInt(R[j]);
             if (L[i] <= R[j]) {
                 A[k] = L[i];
                 i = i + 1;
-            } else { 
+            } else {
                 A[k] = R[j];
                 j = j + 1;
             }
-        }; 
-        return A.slice(0,A.length-1);
+        };
+        console.log(L.join());
+        console.log(R.join());
+        return A;
     })(A, p, q, r);
+};
+Algorithms.sel_sort = function(A) {
+    Array.prototype.max = function() { //max
+        return Math.max.apply(null, this);
+    }
+    return A.max();
 };
 
 
 
 
+Algorithms.pop_sort = function(A) {
+    var temp;
+    for (var i = 0; i < A.length; i++) {
+        var temp = A[i];
+        for (var j = i + 1; j < A.length; j++) {
+            if (A[i] > A[j]) {
+                A[i] = A[j]
+                A[j] = temp;
+            }
+        };
+    };
 
-$(function() { 
-    for (key in Algorithms) { 
-    	var compent = $('.' + key + '>.go');
-    	compent.attr('key',key); 
-        compent.on('click', function() {  
-        	var temp = $(this).attr('key');  
-        	var arr = $('.' + temp + '>.input').val().split(',');
-        	var text = Algorithms[temp](arr);
-            $('.' + temp + '>.result').text(text); 
-        }); 
+    return A;
+
+}
+
+
+$(function() {
+    for (key in Algorithms) {
+        var compent = $('.' + key + '>.go');
+        compent.attr('key', key);
+        compent.on('click', function() {
+            var temp = $(this).attr('key');
+            var arr = $('.' + temp + '>.input').val().split(',');
+            var text = Algorithms[temp](arr);
+            $('.' + temp + '>.result').text(text);
+        });
     }
 
 });
